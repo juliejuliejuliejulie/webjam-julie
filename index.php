d7f7d7eeccd28c35d96846759964279861566d75
Unknown <bouchardon.julie@gmail.com>
2016-11-20 05:14:01 +0100

    Ready !

--- index.php
+++ index.php
@@ -3,19 +3,55 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css?family=Bitter:400,700|Noto+Sans:700" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.css">
     <link rel="stylesheet" href="index.css">
   </head>
 
-  <body>
+<?php
+    if(isset($_GET['insulte'])){
+        echo('<body class="insulte">');
+    }
+    else if(isset($_GET['gentillesse'])){
+        echo('<body class="gentillesse">');
+    }
+    else{
+        echo('<body class="question">');
+    }
+    
+    
+    ?>
+    
+   <section class="insulte question">
     <h1 class="insulte"> TU VEUX TE BATTRE ?</h1>
     <?php
       $insultes=json_decode(file_get_contents('insultes.json'));
       //var_dump($names);
     ?>
     <form>
-        <input class="insulte" type="text" name="insulte" placeholder="Insulte-moi fort">
+        <input class="insulte" type="text" name="insulte" placeholder="Fais moi mal Johnny">
     </form>
-    <?php
+    </section>
+<section class="gentillesse question">
+    <h1 class="gentillesse"> TU VEUX <br>UN CALIN ?</h1>
+     <?php
+      $gentillesses=json_decode(file_get_contents('gentillesses.json'));
+      //var_dump($names);
+    ?>
+    <form>
+        <input class="gentillesse" type="text" name="gentillesse" placeholder="Mots doux">
+    </form>
+    </section>
+    <section class="reponse">
+         <?php
+      if(isset($_GET['gentillesse'])){
+        $content=$_GET['gentillesse'];
+        $insert = new insert();
+        $insert->name=$content;
+        array_push($gentillesses,$insert);
+
+        $index= rand(0,count($gentillesses));
+        echo('<p class="gentillesse">'.$gentillesses[$index]->name.'</p><i class="fa fa-heart" aria-hidden="true"></i>');
+      }
       if(isset($_GET['insulte'])){
         $content=$_GET['insulte'];
         $insert = new insert();
@@ -23,15 +59,10 @@
         array_push($insultes,$insert);
 
         $index= rand(0,count($insultes));
-        echo('<p class="insulte">'.$insultes[$index]->name.'</p>');
+        echo('<p class="insulte">'.$insultes[$index]->name.'</p><i class="fa fa-bolt" aria-hidden="true"></i>');
       }
     ?>
-
-    <h1 class="gentillesse"> FREE HUGS</h1>
-    <form>
-        <input class="gentillesse" type="text" name="gentillesse" placeholder="Ecrivez une gentillesse">
-    </form>
-
+    </section>
     <?php
       class insert {
           public function __construct(array $arguments = array()) {
